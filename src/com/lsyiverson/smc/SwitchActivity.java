50d231b71aa26f470ea82f041f47d6ead97d8d6b
lsyiverson <lsyiverson@gmail.com>
2012-10-23 22:56:56 +0800

    Add a Utils class to operate the mobile data

--- src/com/lsyiverson/smc/SwitchActivity.java
+++ src/com/lsyiverson/smc/SwitchActivity.java
@@ -1,20 +1,53 @@
+
 package com.lsyiverson.smc;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
 
 public class SwitchActivity extends Activity {
+    private static String LOG_TAG = "SwitchActivity";
+
+    private Switch mStartSwitch;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_switch);
+        setupView();
+    }
+
+    @Override
+    protected void onResume() {
+        mStartSwitch.setChecked(isServiceRuning());
+        mStartSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Log.d(LOG_TAG, "switch turn on");
+                } else {
+                    Log.d(LOG_TAG, "switch turn off");
+                }
+            }
+        });
+        super.onResume();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_switch, menu);
+        return true;
+    }
+
+    private void setupView() {
+        mStartSwitch = (Switch)findViewById(R.id.start_switch);
+    }
+
+    private boolean isServiceRuning() {
         return true;
     }
 }
---
+++ src/com/lsyiverson/smc/Utils.java
@@ -0,0 +1,53 @@
+
+package com.lsyiverson.smc;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+public class Utils {
+    /**
+     * Get the mobile data status
+     * 
+     * @param context
+     * @return If the mobile data enabled
+     */
+    private static boolean getMobileDataEnabled(ConnectivityManager cm) {
+        boolean mobileDataEnabled = true;
+        try {
+            Method method = cm.getClass().getMethod("getMobileDataEnabled", null);
+            Object[] arg = null;
+            mobileDataEnabled = (Boolean)method.invoke(cm, arg);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        return mobileDataEnabled;
+    }
+
+    /**
+     * Set the mobile data status
+     * 
+     * @param If the mobile data is enabled
+     */
+    public static void setMobileDataEnabled(boolean enabled, Context context) {
+        ConnectivityManager cm = (ConnectivityManager)context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (enabled) {
+            if (!getMobileDataEnabled(cm)) {
+                // TODO: set status enabled
+            }
+        } else {
+            if (getMobileDataEnabled(cm)) {
+                // TODO: set status disabled
+            }
+        }
+    }
+}
